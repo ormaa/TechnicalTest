c86a63c2321c7af7536da04fa285f3c9501f9a02
olivier Robin <olivier@ormaa.fr>
2018-05-15 17:02:03 +0200

    change highlighted row color

--- DigiSchoolTechnicalTest/Controls/TextView.swift
+++ DigiSchoolTechnicalTest/Controls/TextView.swift
@@ -12,6 +12,17 @@ import UIKit
 @IBDesignable
 class TextView: UITextView{
     
+    // place holder is nice to display what to enter in the textfield, when there is no text inside it.
+    @IBInspectable var placeHolder: String = "" {
+        didSet {
+            // for comparison, let's add a UITextField with a placeholder
+            let nameTextfield: UITextField? = UITextField(frame:
+                CGRect(x: 20, y: 20, width: frame.size.width - 40, height: 40))
+            nameTextfield?.placeholder = placeHolder
+            self.addSubview(nameTextfield!)
+        }
+    }
+    
     @IBInspectable var cornerRadius: CGFloat = 0 {
         didSet {
             layer.cornerRadius = cornerRadius
@@ -31,26 +42,37 @@ class TextView: UITextView{
         }
     }
     
-//    required init?(coder: NSCoder) {
-//        super.init(coder: coder)
-//        
-//    }
-    
+
     #if !TARGET_INTERFACE_BUILDER
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        //listenForTextChangedNotifications()
     }
     
     /** Override common init, for manual allocation */
     override init(frame: CGRect, textContainer: NSTextContainer?) {
         super.init(frame: frame, textContainer: textContainer)
-        //listenForTextChangedNotifications()
     }
     #endif
     
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//    }
     
+    
+
+//    // Add a toolbar with a `Done` button
+//    //
+//    func addDoneToolbar() {
+//        
+//        let toolbar = UIToolbar()
+//        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+//        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(onToolBarDone))
+//        
+//        toolbar.items = [space, doneBtn]
+//        toolbar.sizeToFit()
+//        
+//        inputAccessoryView = toolbar
+//    }
+//    
+//    @objc func onToolBarDone(sender: Any) {
+//         //resignFirstResponder()
+//        self.sendActions(for: .editingDidEndOnExit)
+//    }
 }
--- DigiSchoolTechnicalTest/DetailsViewController.swift
+++ DigiSchoolTechnicalTest/DetailsViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class DetailsViewController: UIViewController {
+class DetailsViewController: UIViewController, UITextViewDelegate{
     
     @IBOutlet weak var movieTitle: UILabel!
     @IBOutlet weak var poster: ImageView!
@@ -23,11 +23,18 @@ class DetailsViewController: UIViewController {
     @IBOutlet weak var castingText: TextView!
     @IBOutlet weak var similarMoviesText: TextView!
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var detailsViewModel: DetailsViewModel? = nil
     
     override func viewDidLoad() {
 
+        // TextView delegate, to get some needed event
+        myReviewText.delegate = self
+        sysnopsisText.delegate = self
+        castingText.delegate = self
+        similarMoviesText.delegate = self
+        
         //gesture recognisers
         let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
         swipeRight.direction = .right
@@ -36,12 +43,59 @@ class DetailsViewController: UIViewController {
         // override with custom navigation bar
         navigationController?.navigationBar.addSubview(DetailsBar_View.instanceFromNib(width: self.view.frame.width))
     
+        
     }
     override func viewWillAppear(_ animated: Bool) {
+
         displayDetails()
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+    }
+
+    func moveViewDelta(deltaY : CGFloat) {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.view!.frame = self.view!.frame.offsetBy(dx: 0, dy: -deltaY)
+        })
+    }
+
+
+    /// textView
+    func textViewDidEndEditing(_ textView: UITextView) {
+        print("begin editing")
+        moveViewDelta(deltaY: 100)
+    }
+    
+//    func textViewDidChange(_ textView: UITextView) {
+//    }
+    
+    // character entered in textView
+    //
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if(text == "\n") {
+            // hide the keyboard
+            textView.resignFirstResponder()
+            moveViewDelta(deltaY: 0)
+            return false
+        }
+        return true
     }
     
     
+    /// Activity indicator
+    
+    func hideActivity() {
+        DispatchQueue.main.async {
+            self.activityIndicator.stopAnimating()
+        }
+    }
+    func showActivity() {
+        DispatchQueue.main.async {
+            self.activityIndicator.startAnimating()
+        }
+    }
     
     // define the dependencies
     //
@@ -57,6 +111,7 @@ class DetailsViewController: UIViewController {
             detailsViewModel!.moviesItem != nil {
                 
             if detailsViewModel!.moviesItem!.poster != nil {
+                // download the image
                 detailsViewModel?.getPosterImage(imageURLString: detailsViewModel!.moviesItem!.poster!, completion: { (error, data) in
                     guard error == "" else {
                         // error : web or parsing error
@@ -66,14 +121,15 @@ class DetailsViewController: UIViewController {
                     if data != nil {
                         let img = UIImage(data: data!)
                         if img != nil {
+                            self.hideActivity()
                             DispatchQueue.main.async {
-                                //self.poster.image = img
+                                self.poster.image = img
                             }
                         }
                     }
                 })
             }
-            
+            // Set title
             movieTitle.text = detailsViewModel!.moviesItem!.title
             
         }
